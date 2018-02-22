9762ed37fc25b6bb6fe0455d71c26b85d2afe60f
Usong Cho <silvermouse4012@gmail.com>
2018-02-21 17:23:47 -0700

    JavaScript_Challenges

---
+++ js_chal.js
@@ -0,0 +1,45 @@
+// function palinDrome(s){
+//   return s == s.split("").reverse().join("")
+// }
+
+// console.log(palinDrome("eve")); 
+// console.log(palinDrome("ball")); 
+
+
+
+// function combinator(s) {
+//   list = []
+//   for(i = 0; i < s.length; i++) {
+//     for(j = i + 1; j < s.length + 1; j++){
+//       list.push(s.slice(i, j))
+//     }
+//   }
+//     return list;
+// }
+// console.log(combinator("dog"));
+
+
+// function alphabet(str) {
+//   return str.split('').sort().join('');
+// };
+// console.log(alphabet("webmaster"));
+
+
+// function upperCase(str) {
+
+//   var arr = str.split(' ');
+//   var newarr = [];
+  
+//   for(var x = 0; x < arr.length; x++) {
+//       newarr.push(arr[x].charAt(0).toUpperCase()+arr[x]
+//     .slice(1));
+//   }
+//   return newarr.join(' ')
+// }
+// console.log(upperCase('boom shaka laka'));
+
+function add(str1, str2){
+  add()= str1 + str2
+};
+
+console.log(add(2, 3));
